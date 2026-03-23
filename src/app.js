67461de06d54dabd047eb0a0b1ee0b05ff177c5a
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2026-03-23 18:25:13 +0530

    implemented ESlint and prettier

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT 
+const PORT = process.env.PORT; 
 
 app.get('/', (req, res) => {
-    res.status(200).send(`hello from http://localhost:${PORT}`)
-})
+  res.status(200).send(`hello from http://localhost:${PORT}`);
+});
 
 export default app;
