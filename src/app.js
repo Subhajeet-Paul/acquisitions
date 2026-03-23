a62edfb19d9c1389ef726523b6819533880c6c80
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2026-03-23 17:48:20 +0530

    setup done

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT 
+
+app.get('/', (req, res) => {
+    res.status(200).send(`hello from http://localhost:${PORT}`)
+})
+
+export default app;
